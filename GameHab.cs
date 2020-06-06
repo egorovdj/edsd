6ea9dcfd50b8b57a83f26ef6d6fd96aa8f143429
egorovdj <66457899+egorovdj@users.noreply.github.com>
2020-06-06 11:49:11 +0300

    Update appsettings.json

--- GameHab.cs
+++ GameHab.cs
@@ -2,30 +2,216 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace edsd
 {
+    [Authorize]
     public class GameHab : Hub
     {
-        static Dictionary<string, string> gamers = new Dictionary<string, string>();
+        static Dictionary<string, Gamer> gamers = new Dictionary<string, Gamer>();
+        static Dictionary<string, Game> games = new Dictionary<string, Game>();
+        static Dictionary<string, int> hands = new Dictionary<string, int> { { "rock", 0 }, { "paper", 1 }, { "scissors", 2 }, { "lizard", 3 }, { "spock", 4 } };
+        static Rule[,] rules = new Rule[5, 5]{
+            { new Rule { Result = 0, Message = "in a draw" }, new Rule { Result = -1, Message = "paper wraps rock" }, new Rule { Result = 1, Message = "rock breaks scissors" }, new Rule { Result = 1, Message = "a rock crushes a lizard" }, new Rule { Result = -1, Message = "spock vaporizes rock" } },
+            { new Rule { Result = 1, Message = "paper wraps the stone" }, new Rule { Result = 0, Message = "in a draw" }, new Rule { Result = -1, Message = "scissors cut paper" }, new Rule { Result = -1, Message = "lizard eats paper" }, new Rule { Result = 1, Message = "paper sets up spock" } },
+            { new Rule { Result = -1, Message = "rock breaks scissors" }, new Rule { Result = 1, Message = "scissors cut paper" }, new Rule { Result = 0, Message = "in a draw" }, new Rule { Result = 1, Message = "scissors cut off the lizard's head" }, new Rule { Result = -1, Message = "spock breaks the scissors" } },
+            { new Rule { Result = -1, Message = "a rock crushes a lizard" }, new Rule { Result = 1, Message = "lizard eats paper" }, new Rule { Result = -1, Message = "scissors cut off the lizard's head" }, new Rule { Result = 0, Message = "in a draw" }, new Rule { Result = 1, Message = "the lizard poisons spock" } },
+            { new Rule { Result = 1, Message = "spock vaporizes rock" }, new Rule { Result = -1, Message = "paper sets up spock" }, new Rule { Result = 1, Message = "spock breaks the scissors" }, new Rule { Result = -1, Message = "the lizard poisons spock" }, new Rule { Result = 0, Message = "in a draw" } }
+        };
         public async void Invite(string UserName)
         {
+            string SelfName = Context.User.Identity.Name;
             if (gamers.ContainsKey(UserName))
-                await this.Clients.Client(gamers[UserName]).SendAsync("Invite", Context.User.Identity.Name);
+                await Clients.Client(gamers[UserName].ConnectionId).SendAsync("Invite", SelfName);
+        }
+        public async void Accept(string UserName)
+        {
+            if (gamers.ContainsKey(UserName))
+            {
+                string SelfName = Context.User.Identity.Name;
+                await Clients.Clients(gamers[UserName].ConnectionId, gamers[SelfName].ConnectionId).SendAsync("Accept", SelfName, UserName);
+                // Preparing the game
+                string GameId = Guid.NewGuid().ToString().ToUpper();
+                Game game = new Game { FirstPlayer = UserName, SecondPlayer = SelfName, FirstPlayerCount = 0, SecondPlayerCount = 0, GameId = GameId };
+                games.Add(GameId, game);
+                gamers[UserName].GameId = GameId;
+                gamers[SelfName].GameId = GameId;
+                //await Clients.Client(gamers[UserName].ConnectionId).SendAsync("Accept", SelfName, false);
+                //await Clients.Client(gamers[SelfName].ConnectionId).SendAsync("Accept", UserName, true);
+                //await Clients.All.SendAsync("InGame", gamers);
+                //System.Threading.Thread.Sleep(5000);
+                //await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("Start");
+            }
+        }
+        public async void Start()
+        {
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName) && gamers[SelfName].GameId != null)
+            {
+                // Preparing the game
+                string GameId = gamers[SelfName].GameId;
+                await Clients.All.SendAsync("InGame", gamers);
+            }
+        }
+        public async void StartInvite()
+        {
+            //System.Threading.Thread.Sleep(5000);
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName) && gamers[SelfName].GameId != null)
+            {
+                string GameId = gamers[SelfName].GameId;
+                games[GameId].FirstPlayerMove = null;
+                games[GameId].SecondPlayerMove = null;
+                await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("Start");
+            }
+        }
+        public async void DidNot(string UserName)
+        {
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(UserName))
+                await Clients.Client(gamers[UserName].ConnectionId).SendAsync("DidNot", SelfName);
+        }
+        public async void StopGame()
+        {
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName) && gamers[SelfName].GameId != null)
+            {
+                string GameId = gamers[SelfName].GameId;
+                if (gamers.ContainsKey(games[GameId].FirstPlayer)) gamers[games[GameId].FirstPlayer].GameId = null;
+                if (gamers.ContainsKey(games[GameId].SecondPlayer)) gamers[games[GameId].SecondPlayer].GameId = null;
+                var lastGame = games[GameId];
+                games.Remove(GameId);
+                await Clients.All.SendAsync("StopGame", gamers, lastGame);
+            }
+        }
+        public async void Move(string hand)
+        {
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName) && gamers[SelfName].GameId != null)
+            {
+                bool isFirst = false;
+                int FirstPlayerMove = -1;
+                int SecondPlayerMove = -1;
+                string GameId = gamers[SelfName].GameId;
+                if (games[GameId].FirstPlayer == SelfName)
+                {
+                    games[GameId].FirstPlayerMove = hand;
+                    FirstPlayerMove = hands[hand];
+                    isFirst = true;
+                }
+                if (games[GameId].SecondPlayer == SelfName)
+                {
+                    games[GameId].SecondPlayerMove = hand;
+                    SecondPlayerMove = hands[hand];
+                }
+                if (games[GameId].FirstPlayerMove != null && games[GameId].SecondPlayerMove != null)
+                {
+                    if (isFirst) SecondPlayerMove = hands[games[GameId].SecondPlayerMove];
+                    else FirstPlayerMove = hands[games[GameId].FirstPlayerMove];
+
+                    var message = rules[FirstPlayerMove, SecondPlayerMove].Message;
+                    var result = rules[FirstPlayerMove, SecondPlayerMove].Result;
+                    if (result > 0) games[GameId].FirstPlayerCount++;
+                    if (result < 0) games[GameId].SecondPlayerCount++;
+                    int maxCount = Math.Max(games[GameId].FirstPlayerCount, games[GameId].SecondPlayerCount);
+                    games[GameId].FirstPlayerMove = null;
+                    games[GameId].SecondPlayerMove = null;
+                    await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("GameRound", message, result, games[GameId].FirstPlayer, games[GameId].SecondPlayer, maxCount);
+                }
+            }
+        }
+        public async void Timeout()
+        {
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName) && gamers[SelfName].GameId != null)
+            {
+                bool isFirst = false;
+                string GameId = gamers[SelfName].GameId;
+                if (games[GameId].FirstPlayer == SelfName)
+                {
+                    games[GameId].FirstPlayerMove = "timeout";
+                    isFirst = true;
+                }
+                if (games[GameId].SecondPlayer == SelfName)
+                {
+                    games[GameId].SecondPlayerMove = "timeout";
+                }
+                if (games[GameId].FirstPlayerMove == "timeout" && games[GameId].SecondPlayerMove == "timeout")
+                {
+                    games[GameId].FirstPlayerMove = null;
+                    games[GameId].SecondPlayerMove = null;
+                    System.Threading.Thread.Sleep(5000);
+                    await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("Start");
+                    return;
+                }
+                if (games[GameId].FirstPlayerMove != null && games[GameId].SecondPlayerMove != null)
+                {
+                    var message = "A timeout occurred!";
+                    var result = isFirst ? -1 : 1;
+                    if (!isFirst) games[GameId].FirstPlayerCount++;
+                    if (isFirst) games[GameId].SecondPlayerCount++;
+                    int maxCount = Math.Max(games[GameId].FirstPlayerCount, games[GameId].SecondPlayerCount);
+                    games[GameId].FirstPlayerMove = null;
+                    games[GameId].SecondPlayerMove = null;
+                    await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("GameRound", message, result, games[GameId].FirstPlayer, games[GameId].SecondPlayer, maxCount);
+                }
+            }
         }
         public override async Task OnConnectedAsync()
         {
-            if (gamers.ContainsKey(Context.User.Identity.Name)) gamers[Context.User.Identity.Name]= Context.ConnectionId;
-            else gamers.Add(Context.User.Identity.Name, Context.ConnectionId);
-            await Clients.All.SendAsync("Login", gamers);
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName)) gamers[SelfName] = new Gamer { ConnectionId = Context.ConnectionId, GameId = null };
+            else gamers.Add(SelfName, new Gamer { ConnectionId = Context.ConnectionId, GameId = null });
+            await Clients.All.SendAsync("InGame", gamers);
             await base.OnConnectedAsync();
         }
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            if (gamers.ContainsKey(Context.User.Identity.Name)) gamers.Remove(Context.User.Identity.Name);
-            await Clients.All.SendAsync("Logout", gamers);
+            string SelfName = Context.User.Identity.Name;
+            if (gamers.ContainsKey(SelfName))
+            {
+                string GameId = gamers[SelfName].GameId;
+                if (games.ContainsKey(GameId)) {
+                    bool isFirst = games[GameId].FirstPlayer == SelfName;
+                    var message = "The enemy ignominiously left the field of battle!";
+                    var result = isFirst ? -1 : 1;
+                    if (!isFirst) games[GameId].FirstPlayerCount += 5;
+                    if (isFirst) games[GameId].SecondPlayerCount += 5;
+                    int maxCount = Math.Max(games[GameId].FirstPlayerCount, games[GameId].SecondPlayerCount);
+                    games[GameId].FirstPlayerMove = null;
+                    games[GameId].SecondPlayerMove = null;
+                    await Clients.Clients(gamers[games[GameId].FirstPlayer].ConnectionId, gamers[games[GameId].SecondPlayer].ConnectionId).SendAsync("GameRound", message, result, games[GameId].FirstPlayer, games[GameId].SecondPlayer, maxCount);
+                    gamers.Remove(SelfName);
+                }
+                else
+                {
+                    gamers.Remove(SelfName);
+                    await Clients.All.SendAsync("InGame", gamers);
+                }
+            }
             await base.OnDisconnectedAsync(exception);
         }
+    }
+    public class Gamer
+    {
+        public string ConnectionId { get; set; }
+        public string GameId { get; set; }
+    }
+    public class Game
+    {
+        public string GameId { get; set; }
+        public string FirstPlayer { get; set; }
+        public string SecondPlayer { get; set; }
+        public string FirstPlayerMove { get; set; }
+        public string SecondPlayerMove { get; set; }
+        public int FirstPlayerCount { get; set; }
+        public int SecondPlayerCount { get; set; }
+    }
+    public class Rule
+    {
+        public int Result { get; set; }
+        public string Message { get; set; }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -47,55 +47,171 @@ $(() => {
         });
     };
 
-    $('#controlUnit button').on('click', async function (e) {
-        let thet = $(this);
-        this.blur();
-        thet.siblings().find('i').removeClass('fas').addClass('far');
-        thet.find('i').removeClass('fas').addClass('far');
-        if (await confirm(`You chose <strong>${this.dataset.action}</strong>. Respond with this move?`)) thet.find('i').toggleClass('fas', 'far');
-    });
-    $('#users a.dropdown-item').on('click', async function (e) {
-        e.preventDefault();
-        let thet = $(this);
-        if (thet.find('i').hasClass('text-danger')) await alert(`The user is not in the game!`);
-        else if (document.getElementById('SelfName').value.trim() == this.textContent.trim()) await alert(`You don't have to play with yourself!`);
-        else {
-            hubConnection.invoke('invite', this.textContent.trim());
-            await alert(`A request for the game was sent to the user <a href='mailto:${this.textContent}'>${this.textContent}</a>. We are waiting for his response...`);
+    if (document.getElementById('SelfName').value.trim() && location.pathname == '/') {
+        var timer = () => {
+            let progress = document.getElementById('progress');
+            timer.counter = 30;
+            timer.reset();
+            timer.interval = setInterval(() => {
+                if (--timer.counter < 0) {
+                    if (!timer.prevent) {
+                        hubConnection.invoke('Timeout').catch(errorNotify);
+                        alert('You allowed a timeout!')
+                    }
+                    timer.reset();
+                    return;
+                }
+                if (timer.counter == 20) {
+                    progress.classList.remove('bg-success');
+                    progress.classList.add('bg-warning');
+                }
+                if (timer.counter == 10) {
+                    progress.classList.remove('bg-warning');
+                    progress.classList.add('bg-danger');
+                }
+                progress.style.width = `${(30 - timer.counter) / 30 * 100}%`;
+                progress.ariaValuenow = timer.counter;
+                progress.textContent = timer.counter;
+            }, 1000);
         }
-    });
-    $('header a:not(#users a), footer a').on('click', async function (e) {
-        if (document.getElementById('SelfName').value.trim() && location.pathname == '/') {
-            let target = $(`<div id="linkHidden" style="display:none">${e.currentTarget.outerHTML}</div>`)
+        timer.reset = () => {
+            let progress = document.getElementById('progress');
+            clearInterval(timer.interval);
+            timer.counter = 30;
+            progress.style.width = '0%';
+            progress.className = "progress-bar bg-success";
+            progress.ariaValuenow = "0";
+            progress.textContent = "0";
+            delete timer.prevent;
+        }
+        $('#controlUnit button').on('click', async function (e) {
+            let thet = $(this);
+            this.blur();
+            thet.siblings().find('i').removeClass('fas').addClass('far');
+            thet.find('i').removeClass('fas').addClass('far');
+            if (await confirm(`You chose <strong>${this.dataset.action}</strong>. Respond with this move?`)) {
+                thet.find('i').toggleClass('fas', 'far');
+                hubConnection.invoke('Move', this.dataset.action).catch(errorNotify);
+                timer.prevent = true;
+            }
+        });
+        $('#users a.dropdown-item').on('click', async function (e) {
             e.preventDefault();
-            if (await confirm(`Do you really want to leave the game?`)) {
-                $('body').append(target);
-                $('#linkHidden > *')[0].click();
+            let thet = $(this);
+            if (thet.find('i').hasClass('text-danger')) await alert(`The user is not in the game!`);
+            else if (thet.find('i').hasClass('text-secondary')) await alert(`The user is playing!`);
+            else if (document.getElementById('SelfName').value.trim() == this.textContent.trim()) await alert(`You don't have to play with yourself!`);
+            else {
+                hubConnection.invoke('invite', this.textContent.trim()).catch(errorNotify);
+                await alert(`A request for the game was sent to the user <a href='mailto:${this.textContent}'>${this.textContent}</a>. We are waiting for his response...`);
+            }
+        });
+
+        var errorNotify = async function (err) {
+            if (document.getElementById('SelfName').value.trim() && location.pathname == '/') {
+                alert(err.toString());
+                return console.error(err.toString());
             }
         }
-    });
-    const hubConnection = new signalR.HubConnectionBuilder()
-        .withUrl("/game", { transport: signalR.HttpTransportType.WebSockets | signalR.HttpTransportType.LongPolling | signalR.HttpTransportType.ServerSentEvents})
-        .build();
+        $('header a:not(#users a), footer a').on('click', async function (e) {
+            if (document.getElementById('SelfName').value.trim() && location.pathname == '/') {
+                let target = $(`<div id="linkHidden" style="display:none">${e.currentTarget.outerHTML}</div>`)
+                e.preventDefault();
+                if (await confirm(`Do you really want to leave the game?`)) {
+                    hubConnection.completeClose(new Error("Manual stop"));
+                    $('body').append(target);
+                    $('#linkHidden > *')[0].click();
+                }
+            }
+        });
 
-    hubConnection.on("invite", async function (un) {
-        if (await confirm(`Does <a href='mailto:${un}'>${un}</a> invite you to play?`)) {
-        }
-    });
-    hubConnection.on("login", function (gamers) {
-        for (let el of $('#users .dropdown-item')) {
-            let un = el.textContent.trim();
-            if (gamers[un]) el.innerHTML = `<i class="fas fa-user-check text-success"></i> ${un}`;
-            else el.innerHTML = `<i class="fas fa-user-minus text-danger"></i> ${un}`;
-        }
-    });
-    hubConnection.on("logout", function (gamers) {
-        for (let el of $('#users .dropdown-item')) {
-            let un = el.textContent.trim();
-            if (gamers[un]) el.innerHTML = `<i class="fas fa-user-check text-success"></i> ${un}`;
-            else el.innerHTML = `<i class="fas fa-user-minus text-danger"></i> ${un}`;
-        }
-    });
+        const hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl("/game", { transport: signalR.HttpTransportType.WebSockets | signalR.HttpTransportType.LongPolling | signalR.HttpTransportType.ServerSentEvents })
+            .withAutomaticReconnect()
+            .build();
 
-    hubConnection.start();
+        hubConnection.on("invite", async function (un) {
+            if (await confirm(`Does <a href='mailto:${un}'>${un}</a> invite you to play?`)) {
+                hubConnection.invoke('Accept', un).catch(errorNotify);
+            }
+            else hubConnection.invoke('DidNot', un).catch(errorNotify);
+        });
+        var enemysName = null;
+        var initH2;
+        hubConnection.on("Accept", async function (self, un) {
+            $('#PromiseAlert button').click();
+            enemysName = un;
+            let h2 = $('main h2');
+            initH2 = h2.html();
+            if (self != document.getElementById('SelfName').value.trim()) {
+                h2.html(`Playing with <small>${un}</small>`);
+                await alert(`The user <a href='mailto:${un}'>${un}</a>: I accepted the call...`);
+                hubConnection.invoke('Start').catch(errorNotify);
+            }
+            else {
+                h2.html(`Playing with <small>${self}</small>`);
+                hubConnection.invoke('StartInvite').catch(errorNotify);
+            }
+        });
+        hubConnection.on("Start", async function () {
+            $('#PromiseAlert button').click();
+            document.querySelector('#users .dropdown-toggle').disabled = true;
+            timer();
+        });
+        hubConnection.on("GameRound", async function (message, result, first, second, maxCount) {
+            let res;
+            result = first == document.getElementById('SelfName').value.trim() ? result : -result;
+            switch (result) {
+                case 1:
+                    res = 'You won!';
+                    break;
+                case -1:
+                    res = "You've lost!";
+                    break;
+                case 0:
+                    res = 'Draw!';
+                    break;
+            }
+            timer.reset();
+            setTimeout(() => {
+                if (maxCount >= 5) hubConnection.invoke('StopGame').catch(errorNotify);
+                else hubConnection.invoke('StartInvite').catch(errorNotify);
+            }, 3000);
+            await alert(`<h3>Result of the game round:</h3><p>${message}</p><p>${res}</p>`);
+            let log = $('main .row div:last');
+            log.append(`<hr/><p>${message}</p><p>${res}</p>`)
+        });
+        hubConnection.on("StopGame", async function (gamers, game) {
+            let self = document.getElementById('SelfName').value.trim();
+            if (self == game.firstPlayer || self == game.secondPlayer) {
+                $('#PromiseAlert button').click();
+                await alert(`<h3>Result of the game:</h3><p>The user <a href='mailto:${game.firstPlayer}'>${game.firstPlayer}</a>${game.firstPlayerCount > game.secondPlayerCount ? ' win the game' : ' lost the game'}! Has ${game.firstPlayerCount} wins.</p>
+<p>The user <a href='mailto:${game.secondPlayer}'>${game.secondPlayer}</a>${game.firstPlayerCount < game.secondPlayerCount ? ' win the game' : ' lost the game'}! Has ${game.secondPlayerCount} wins.</p>`);
+                $('main h2').html(initH2);
+                $('main .row div:last').html('');
+                document.querySelector('#users .dropdown-toggle').disabled = false;
+            }
+            for (let el of $('#users .dropdown-item')) {
+                let un = el.textContent.trim();
+                if (gamers[un] && gamers[un].gameId) el.innerHTML = `<i class="fas fa-user-lock text-secondary"></i> ${un}`;
+                else if (gamers[un]) el.innerHTML = `<i class="fas fa-user-check text-success"></i> ${un}`;
+                else el.innerHTML = `<i class="fas fa-user-minus text-danger"></i> ${un}`;
+            }
+        });
+        hubConnection.on("DidNot", async function (un) {
+            $('#PromiseAlert button').click();
+            await alert(`The user <a href='mailto:${un}'>${un}</a> refused to play the game...`);
+        });
+        hubConnection.on("InGame", function (gamers) {
+            for (let el of $('#users .dropdown-item')) {
+                let un = el.textContent.trim();
+                if (gamers[un] && gamers[un].gameId) el.innerHTML = `<i class="fas fa-user-lock text-secondary"></i> ${un}`;
+                else if (gamers[un]) el.innerHTML = `<i class="fas fa-user-check text-success"></i> ${un}`;
+                else el.innerHTML = `<i class="fas fa-user-minus text-danger"></i> ${un}`;
+            }
+        });
+
+        hubConnection.start().then(function () {
+        }).catch(errorNotify);
+    }
 });
